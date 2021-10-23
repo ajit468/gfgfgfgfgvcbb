2ac85a4a9702dc593bade79a819b8271930b4646
ajit468 <40148324+ajit468@users.noreply.github.com>
2021-10-24 01:17:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 console.log("Auto roll extension is running...")
 
 setTimeout(() => {
-    document.getElementById("pwc_input").value = "1";
-    document.getElementById("free_play_form_button").click()
+    document.getElementById("roll-button").click()
   }, 6000
 )
