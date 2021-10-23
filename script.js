99ddd3f20ab65b29529f2cf101961232c3ec4fb3
ajit468 <40148324+ajit468@users.noreply.github.com>
2021-10-24 02:11:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("Auto roll extension is running...")
 
 setTimeout(() => {
+    document.getElementsByName('Time to Roll!').reload(true);
     document.getElementById("roll-button").click()
   }, 6000
 )
